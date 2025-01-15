1399c1e74fa80d13ae04561a8548f027ac697c8d
Nugine <nugine@foxmail.com>
2025-01-15 18:22:25 +0800

    ci dashboard

---
+++ dashboard.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+REPO_LIST = [
+    "Nugine/s3s",
+    "Nugine/simd",
+    "Nugine/rlimit",
+    "Nugine/const-str",
+    "Nugine/zuc",
+    "Nugine/numeric_cast",
+    "Nugine/asc",
+    "Nugine/outref",
+    "Nugine/std-next",
+    "Nugine/rust-utils",
+    "Nugine/simdutf-rs",
+    "Nugine/epkv",
+]
+
+
+def main():
+    print("|Repo|Status|")
+    print("|-|-|")
+    for repo in REPO_LIST:
+        link = f"[{repo}](https://github.com/{repo})"
+        ci = f"[![CI](https://github.com/{repo}/actions/workflows/ci.yml/badge.svg)](https://github.com/{repo}/actions)"
+        print(f"|{link}|{ci}|")
+
+
+if __name__ == "__main__":
+    main()
